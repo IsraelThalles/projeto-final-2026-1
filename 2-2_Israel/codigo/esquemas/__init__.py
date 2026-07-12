69ddd3109a27c41180ca072a4fb384e23f07f6d2
Israel Thalles <santosisrael14@gmail.com>
2026-07-12 20:04:19 -0300

    Adiciona o arquivo __init__.py com importações e definição de __all__ para esquemas

---
+++ 2-2_Israel/codigo/esquemas/__init__.py
@@ -0,0 +1,15 @@
+from .esquemas import (
+    NivelDaOfensa,
+    Acao,
+    ComentarioEntrada,
+    RespostaModeracao,
+    ComentarioSaidaBanco
+)
+
+__all__ = [
+    "NivelDaOfensa",
+    "Acao",
+    "ComentarioEntrada",
+    "RespostaModeracao",
+    "ComentarioSaidaBanco"
+]
